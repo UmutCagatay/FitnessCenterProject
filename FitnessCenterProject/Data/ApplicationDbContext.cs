7639198d546935ad18bcfd29e8345ee061b26163
UmutCagatay <umutcancagatay@gmail.com>
2025-12-02 19:42:39 +0300

    DbContext connection

---
+++ FitnessCenterProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FitnessCenterProject.Models;
+
+namespace FitnessCenterProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<TrainerService> TrainerServices { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TrainerService>()
+                .HasKey(ts => new { ts.TrainerId, ts.ServiceId });
+            modelBuilder.Entity<TrainerService>()
+                .HasOne(ts => ts.Trainer)
+                .WithMany(t => t.TrainerServices)
+                .HasForeignKey(ts => ts.TrainerId);
+            modelBuilder.Entity<TrainerService>()
+                .HasOne(ts => ts.Service)
+                .WithMany(s => s.TrainerServices)
+                .HasForeignKey(ts => ts.ServiceId);
+        }
+    }
+}
