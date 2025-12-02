e8661b174a56989bc8390e03776a77de8749dc13
UmutCagatay <umutcancagatay@gmail.com>
2025-12-02 20:46:15 +0300

    Service controller and view added.

---
+++ FitnessCenterProject/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FitnessCenterProject.Data;
+using FitnessCenterProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessCenterProject.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ServiceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var services = await _context.Services.ToListAsync();
+            return View(services);
+        }
+    }
+}
