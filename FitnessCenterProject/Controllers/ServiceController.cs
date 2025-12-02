ae18144e8d358219d9d33b10ee2f5fbfc3682f0d
UmutCagatay <umutcancagatay@gmail.com>
2025-12-02 22:44:55 +0300

    Edit and Delete, general CRUD operation added and updated.

--- FitnessCenterProject/Controllers/ServiceController.cs
+++ FitnessCenterProject/Controllers/ServiceController.cs
@@ -36,14 +36,90 @@ namespace FitnessCenterProject.Controllers
                 return RedirectToAction(nameof(Index));
             }
 
-            var errorMessages = ModelState.Values
+            /* var errorMessages = ModelState.Values
                                           .SelectMany(v => v.Errors)
                                           .Select(e => e.ErrorMessage)
                                           .ToList();
 
             var fullErrorText = string.Join(" | ", errorMessages);
 
-            throw new Exception("Validasyon Hatası Yakalandı: " + fullErrorText);
+            throw new Exception("Validasyon Hatası Yakalandı: " + fullErrorText); */
+
+            return View(service);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                throw new Exception("Güncellenecek hizmet ID'si bulunamadı!");
+            }
+
+            var service = await _context.Services.FindAsync(id);
+
+            if (service == null)
+            {
+                throw new Exception($"{id} numaralı hizmet veritabanında bulunamadı!");
+            }
+
+            return View(service);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Service service)
+        {
+            if (id != service.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(service);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Services.Any(e => e.Id == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var service = await _context.Services.FindAsync(id);
+            if (service == null) return NotFound();
+
+            return View(service);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var service = await _context.Services.FindAsync(id);
+            if (service != null)
+            {
+                _context.Services.Remove(service);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
